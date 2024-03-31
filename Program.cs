4a9613bfdfc66325d6f6256ac34cbdac1e4562c4
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-03-31 11:29:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var franct1 = new Fraction(3, 2);
+            var franct2 = new Fraction(4, 3);
+            var complx1 = new СmplxNum(2, 4, "+");
+            var complx2 = new СmplxNum(3, 1, "+");
+            var birthday1 = new BirthDay(12,04,1999);
+            ClFranction cl = new ClFranction();
+            ClСmplxNum cl2 = new ClСmplxNum();
+            ClBirthDay cl3 = new ClBirthDay();
+            int whil = 0;
+            while (whil <= 0)
+            {
+                Console.WriteLine("\nChoose Task 1-3 or 4 to exit: ");
+                int swh = int.Parse(Console.ReadLine());
+                switch (swh)
+                {
+                    case 1:
+                        Console.WriteLine("Frunct 1: " + franct1.ToString());
+                        Console.WriteLine("Frunct 2: " + franct2.ToString());
+                        cl.Add(franct1, franct2);
+                        cl.Subtraction(franct1, franct2);
+                        cl.Multi(franct1, franct2);
+                        cl.Division(franct1, franct2);
+                        break;
+                    case 2:
+                        Console.WriteLine("Complex Number 1: " + complx1.ToString());
+                        Console.WriteLine("Complex Number 2: " + complx2.ToString());
+                        cl2.Add(complx1, complx2);
+                        cl2.Subtraction(complx1, complx2);
+                        cl2.Multi(complx1, complx2);
+                        cl2.Division(complx1, complx2);
+                        break;
+                    case 3:
+                        Console.WriteLine("Birth Day: " + birthday1.ToString());
+                        cl3.NextWeekInYear(birthday1);
+                        cl3.NextDayInWeek(birthday1,2024);
+                        cl3.DayToBirthDay(birthday1);
+                        break;
+                    case 4:
+                        whil = 1;
+                        break;
+                }
+            }
+        }
+    }
+}
+/*var point = new Point(5, 7);
+            Console.WriteLine(point.ToString());
+
+            var today = DayOfWeek.Sunday;
+            Console.WriteLine(today);
+
+            var user = new User()
+            {
+                Name = "Test",
+                Email = "Test@gmail.com",
+                Age = 25,
+                DayOfWeek = DayOfWeek.Sunday
+            };
+            if(user.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine(user.Name);
+            }
+        */
+/*class User
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public int Age {  get; set; }
+            public DayOfWeek DayOfWeek { get; set; }
+        }
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public override string ToString() => $"{X},{Y}";
+        }*/
---
+++ cmplxNum.cs
@@ -0,0 +1,97 @@
+﻿
+namespace ConsoleApp11
+{
+    struct СmplxNum
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string C { get; set; } 
+        public СmplxNum(double x, double y, string c)
+        {
+            X = x;
+            Y = y;
+            C = c;
+            if (c == "+" | c == "-")
+            {
+
+            }
+            else Console.WriteLine("ERROR");
+        }
+        public override string ToString() => $"{X}{C}{Y}i";
+    }
+    class ClСmplxNum
+    {
+        public void Add(СmplxNum a, СmplxNum b)
+        {
+            if (a.C == "-")
+            {
+                a.Y *= -1;
+            }
+            if (b.C == "-")
+            {
+                b.Y *= -1;
+            }
+            a.X += b.X;
+            a.Y += b.Y;
+            Console.WriteLine(a.ToString());
+        }
+        public void Subtraction(СmplxNum a, СmplxNum b)
+        {
+            if (a.C == "-")
+            {
+                a.Y *= -1;
+            }
+            if (b.C == "-")
+            {
+                b.Y *= -1;
+            }
+            a.X -= b.X;
+            a.Y -= b.Y;
+            Console.WriteLine(a.ToString());
+        }
+        public void Multi(СmplxNum a, СmplxNum b)
+        {
+            if (a.C == "-")
+            {
+                a.Y *= -1;
+            }
+            if (b.C == "-")
+            {
+                b.Y *= -1;
+            }
+            double num1, num2, num3, num4;
+            num1 = a.X * b.X;
+            num2 = a.X * b.Y;
+            num3 = a.Y * b.X;
+            num4 = a.Y * b.Y;
+            a.X = num1 - num4;
+            a.Y = num2 + num3;
+            Console.WriteLine(a.ToString());
+        }
+        public void Division(СmplxNum a, СmplxNum b)
+        {
+            if (a.C == "-")
+            {
+                a.Y *= -1;
+            }
+            if (b.C == "-")
+            {
+                b.Y *= -1;
+            }
+            double num1, num2, num3, num4, num5, num6;
+            double x1, x2, x3;
+            num1 = a.X * b.X;
+            num2 = a.X * b.Y;
+            num3 = a.Y * b.X;
+            num4 = a.Y * b.Y;
+            num5 = num1 - num4;
+            num6 = num3 - num2;
+            x1 = a.X * b.X;
+            x2 = a.Y * b.Y;
+            x3 = x1 + x2;
+            a.X = num5 / x3;
+            a.Y = num6;
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
